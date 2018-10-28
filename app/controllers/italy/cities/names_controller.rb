e7af0832314bc4f211c6bc563bab56c9205f3e8c
Mat <matteo.grassotti@gmail.com>
2018-10-29 00:18:15 +0100

    HTML views for names #index and #show

--- app/controllers/italy/cities/names_controller.rb
+++ app/controllers/italy/cities/names_controller.rb
@@ -1,11 +1,18 @@
 class Italy::Cities::NamesController < ApplicationController
   def index
-    render json: City.pluck(:nome)
+    @names = City.pluck(:nome)
+    respond_to do |format|
+      format.html
+      format.json { render json: @names }
+    end
   end
 
   def show
-    city = City.where(nome: /^#{Regexp.escape(params[:id])}$/i).first
-    raise Mongoid::Errors::DocumentNotFound.new(City, params[:id]) unless city
-    render json: city.to_json(:except => :_id) 
+    @city = City.where(nome: /^#{Regexp.escape(params[:id])}$/i).first
+    raise Mongoid::Errors::DocumentNotFound.new(City, params[:id]) unless @city
+    respond_to do |format|
+      format.html
+      format.json { render json: @city.to_json(:except => :_id) }
+    end
   end
 end
